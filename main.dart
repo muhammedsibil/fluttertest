3be9e0b5721fcfe7d9d48e7c90a66b0e400a814a
muhammed sibil k <78753972+muhammedsibil@users.noreply.github.com>
2021-02-09 00:08:52 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertest/StoreHome/storehome.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: StoreHome(),
+  ));
+}
+
